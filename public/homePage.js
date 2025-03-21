4ecca1163d17c2a4f61f830a9c773913041846ad
Regina Salikhova <yanadera1873@gmail.com>
2025-03-21 15:47:40 +0400

    Доделала функционал страницы «Личный кабинет пользователя»(операции с деньгами и работа с избранным)

--- public/homePage.js
+++ public/homePage.js
@@ -33,16 +33,76 @@ updateExchangeRate();
 const intervalId = setInterval(updateExchangeRate, 1000);
 
 // Операции с деньгами
+  //Пополнение
 const moneyManager = new MoneyManager();
 moneyManager.addMoneyCallback = (data) => {
   ApiConnector.addMoney(data, (response) => {
     if(response.success) {
       ProfileWidget.showProfile(response.data);
-      this.setMessage(isSuccess, message);
+      moneyManager.setMessage(response.success, "Баланс успешно пополнен.");
     } else {
-      this.setMessage(isSuccess, message);
+      moneyManager.setMessage(response.success, response.error);
+    }
+  });
+};
+  // Конвертация
+moneyManager.conversionMoneyCallback = (data) => {
+  ApiConnector.convertMoney(data, (response) => {
+    if(response.success) {
+      ProfileWidget.showProfile(response.data);
+      moneyManager.setMessage(response.success, "Валюта успешно сконвертирована.");
+    } else {
+      moneyManager.setMessage(response.success, "response.error");
+    }
+  });
+};
+  // Перевод
+moneyManager.sendMoneyCallback = (data) => {
+  ApiConnector.transferMoney(data, (response) => {
+    if(response.success) {
+      ProfileWidget.showProfile(response.data);
+      moneyManager.setMessage(response.success, "Перевод средств успешно выполнен");
+    } else {
+      moneyManager.setMessage(response.success, response.error);
     }
   });
 };
 
-// далее 3 пункт
+// Работа с избранным
+  // Начальный список избранного
+const favoritesWidget = new FavoritesWidget();
+ApiConnector.getFavorites((response) => {
+  if(response.success) {
+    favoritesWidget.clearTable();
+    favoritesWidget.fillTable(response.data);
+    moneyManager.updateUsersList(response.data);
+  }
+});
+
+  // Добавления пользователя в список избранных
+favoritesWidget.addUserCallback = ({id, name}) => {
+  ApiConnector.addUserToFavorites({id, name}, (response) => {
+    if(response.success) {
+      favoritesWidget.clearTable();
+      favoritesWidget.fillTable(response.data);
+      moneyManager.updateUsersList(response.data);
+      favoritesWidget.setMessage(response.success, `Пользователь ${name} успешно добавлен в список избранного`);
+    } else {
+      favoritesWidget.setMessage(response.success, response.error);
+    }
+  });
+};
+
+  // Удаление пользователя из избранного
+  favoritesWidget.removeUserCallback = (id) => {
+    ApiConnector.removeUserFromFavorites(id, (response) => {
+      if(response.success) {
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(response.data);
+        moneyManager.updateUsersList(response.data);
+        favoritesWidget.setMessage(response.success, "Пользователь успешно удалён из списка избранного");
+      } else {
+        favoritesWidget.setMessage(response.success, response.error);
+      }
+    });
+  }
